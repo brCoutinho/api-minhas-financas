d2ba2ca0da0c9733e40a0f0f4c38cb7a71f563e7
Thiago Coutinho <thcoutinho1997@gmail.com>
2021-07-03 13:05:20 -0300

    First commit - config

---
+++ src/test/java/com/brcoutinho/minhasfinancas/MinhasFinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brcoutinho.minhasfinancas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhasFinancasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
